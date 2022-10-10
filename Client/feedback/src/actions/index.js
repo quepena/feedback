e5804e0e2784c0f8c587fa1c31600259df0b002e
quepena <fishalx6263@gmail.com>
2022-10-10 16:46:50 +0300

    netlify

--- Client/feedback/src/actions/index.js
+++ Client/feedback/src/actions/index.js
@@ -2,7 +2,7 @@ import { ADD_FEEDBACK_SUCCESS } from "../constants/index.js"
 import axios from "axios"
 
 export const addFeedback = (form) => async (dispatch) => {
-  const { data } = await axios.post('http://localhost:3001/createFeedback', form);
+  const { data } = await axios.post('https://feedback-react-nest.herokuapp.com/createFeedback', form);
 
   dispatch({
     type: ADD_FEEDBACK_SUCCESS,
